63aad061a3cbc40f24cb3e7999a51d359af81408
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-05-11 16:36:33 +0200

    fixed joint mapping between solver and joint index with fixed joints

--- src/wbk_sim/robot_base.py
+++ src/wbk_sim/robot_base.py
@@ -20,24 +20,29 @@ class RobotBase:
         self._joint_state_shape = self.get_joint_state()
         self._joint_name_to_index = {}
         self._link_name_to_index = {}
+        kinematic_solver_map = []
         self._lower_joint_limit = np.zeros(p.getNumJoints(self.urdf))
         self._upper_joint_limit = np.zeros(p.getNumJoints(self.urdf))
 
         for joint_number in range(p.getNumJoints(self.urdf)):
-            joint_name = p.getJointInfo(self.urdf,joint_number)[1].decode("utf-8")
-            self._joint_name_to_index[joint_name]=joint_number
-
             link_name = p.getJointInfo(self.urdf,joint_number)[12].decode("utf-8")
             self._link_name_to_index[link_name]=joint_number
 
-            lower_limit = p.getJointInfo(self.urdf, joint_number)[8]
-            upper_limit = p.getJointInfo(self.urdf, joint_number)[9]
-            if upper_limit < lower_limit:
-                lower_limit = -np.inf
-                upper_limit = np.inf
-            self._lower_joint_limit[joint_number] = lower_limit
-            self._upper_joint_limit[joint_number] = upper_limit
+            if p.getJointInfo(self.urdf,joint_number)[2] is not 4:
+                joint_name = p.getJointInfo(self.urdf,joint_number)[1].decode("utf-8")
+                self._joint_name_to_index[joint_name]=joint_number
+
+                kinematic_solver_map.append(joint_number)
+
+                lower_limit = p.getJointInfo(self.urdf, joint_number)[8]
+                upper_limit = p.getJointInfo(self.urdf, joint_number)[9]
+                if upper_limit < lower_limit:
+                    lower_limit = -np.inf
+                    upper_limit = np.inf
+                self._lower_joint_limit[joint_number] = lower_limit
+                self._upper_joint_limit[joint_number] = upper_limit
         
+        self._kinematic_solver_map = np.array(kinematic_solver_map)
 
         self.max_joint_force = 1000*np.ones(p.getNumJoints(self.urdf))
         for joint_number in range(p.getNumJoints(self.urdf)):
@@ -52,7 +57,7 @@ class RobotBase:
         """
         joint_state = {}
         for joint_number in range(p.getNumJoints(self.urdf)):
-            if p.getJointInfo(self.urdf,joint_number)[2] is not p.JOINT_FIXED:
+            if p.getJointInfo(self.urdf,joint_number)[2] is not 4:
                 joint = p.getJointInfo(self.urdf,joint_number)[1].decode("utf-8")#convert byte string to string
                 joint_position = p.getJointState(self.urdf, joint_number)[0]
                 joint_velocity = p.getJointState(self.urdf, joint_number)[1]
@@ -131,13 +136,15 @@ class RobotBase:
                                                         upperLimits = self._upper_joint_limit)
 
             # compute forward kinematics for solution
-            for joint_number in range(p.getNumJoints(self.urdf)):
-                p.resetJointState(self.urdf,joint_number,joint_poses[joint_number])
+            for index in range(len(self._joint_state_shape)):
+                joint_number = self._kinematic_solver_map[index]
+                p.resetJointState(self.urdf,joint_number,joint_poses[index])
             current_position,_ = self.get_endeffector_pose(endeffector)
             if np.linalg.norm(target_position-current_position):
                 break
-                
-        for joint_number, joint_position in enumerate(joint_poses):
+    
+        for index, joint_position in enumerate(joint_poses):
+            joint_number = self._kinematic_solver_map[index]
             p.setJointMotorControl2(self.urdf, joint_number, p.POSITION_CONTROL,
                                             force=self.max_joint_force[joint_number],
                                             targetPosition=joint_position)
@@ -187,7 +194,7 @@ if __name__ == "__main__":
     dirname = os.path.dirname(__file__)
     parentDir = os.path.dirname(dirname)
     urdf_file1 = os.path.join(
-            dirname, 'robot_descriptions', 'igus_4DOF_SV.urdf')
+            dirname, 'robot_descriptions', 'comau_NJ290_3-0_m.urdf')
 
     physics_client = p.connect(p.GUI)
     p.setPhysicsEngineParameter(numSolverIterations=1000)
@@ -204,11 +211,14 @@ if __name__ == "__main__":
     
 
     p.setRealTimeSimulation(1)
-    
-    for i in range(100):
-        #target_state = {'q1':0,'q2':0.5,'q3':-0.5,'q4':0.5,'q5':0,'q6':0}
-        target_pose = np.array([i/200,-i/200,0.3])
-        robot.set_endeffector_pose('link4',target_pose)
-        time.sleep(0.2)
-        current_pose, target_orientation =robot.get_endeffector_pose('link4')
-        print(current_pose-target_pose)
+    print(robot._joint_name_to_index)
+    for i in range(1000): 
+        target_state = {'q1':i/100.0,'q2':i/50,'q3':i/20,'q4':i/10,'q5':i/5,'q6':i} 
+        #robot.set_joint_position(target_state) 
+        target_orientation = p.getQuaternionFromEuler([0, i/100, 0]) 
+        target_pose = [1.9,0,1.2] 
+        robot.set_endeffector_pose('link6',target_pose,target_orientation,iterations=1) 
+        #print(robot._joint_name_to_index)
+        print(robot.get_endeffector_pose('link6')) 
+        #print(robot.get_joint_state())
+        time.sleep(0.2) 

905c4960c2a371c3ce3412084d2095761bd77606
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-05-12 10:37:13 +0200

    made igus demo into endless loop

--- examples/igus_pos_ctr.py
+++ examples/igus_pos_ctr.py
@@ -7,7 +7,7 @@ import rolap
 if __name__ == "__main__":
     dirname = os.path.dirname(__file__)
     parentDir = os.path.dirname(dirname)
-    urdf_file1 = os.path.join( parentDir,'src','wbk_sim','robot_descriptions', 'igus_4DOF_SV.urdf')
+    urdf_file1 = os.path.join(parentDir,'src','wbk_sim','robot_descriptions', 'igus_4DOF_SV.urdf')
 
     physics_client = p.connect(p.GUI)
     p.setPhysicsEngineParameter(numSolverIterations=1000)
@@ -21,7 +21,8 @@ if __name__ == "__main__":
     target_position = [0.-0.05, 0.3]
     test_path = rolap.build_lemniscate_path(target_position,400,0,0.3)
     wbk.draw_path(test_path)
-    for i in range(400): 
-        target_orientation = p.getQuaternionFromEuler([0, 0, 0]) 
-        robot.set_endeffector_pose('link4',test_path[:,i]) 
-        time.sleep(0.05) 
+    while True:
+        for i in range(400): 
+            target_orientation = p.getQuaternionFromEuler([0, 0, 0]) 
+            robot.set_endeffector_pose('link4',test_path[:,i]) 
+            time.sleep(0.005) 

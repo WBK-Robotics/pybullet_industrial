86cfa9e52bfbe8792d905366be84f86c164f6558
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2022-05-17 11:56:13 +0200

    applied strict pep8 formatting for consistency

--- examples/lemniscate.py
+++ examples/lemniscate.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def build_lemniscate_path(midpoint,steps,height,length):
+
+def build_lemniscate_path(midpoint, steps, height, length):
     """Function which builds a figure 8 path
 
     Args:
@@ -13,9 +14,11 @@ def build_lemniscate_path(midpoint,steps,height,length):
         [type]: [description]
     """
     path = np.zeros((3, steps))
-    path[2,:]=height
+    path[2, :] = height
     for i in range(steps):
         path_state = 1/steps*i*2*np.pi
-        path[0,i]= length * np.cos(path_state)/(1+np.sin(path_state)**2)+midpoint[0]
-        path[1,i]= length * np.sin(path_state) * np.cos(path_state)/(1+np.sin(path_state)**2)+midpoint[1]
+        path[0, i] = length * np.cos(path_state) / \
+            (1+np.sin(path_state)**2)+midpoint[0]
+        path[1, i] = length * np.sin(path_state) * np.cos(path_state) / \
+            (1+np.sin(path_state)**2)+midpoint[1]
     return path
--- src/wbk_sim/utility.py
+++ src/wbk_sim/utility.py
@@ -1,7 +1,8 @@
 import pybullet as p
 import numpy as np
 
-def draw_point(point, color=[0.0,1.0,0.0], length = 0.05,width=2.0):
+
+def draw_point(point, color=[0.0, 1.0, 0.0], length=0.05, width=2.0):
     """Draws a point in the worldspace as a cross of 3 lines
 
     Args:
@@ -10,21 +11,21 @@ def draw_point(point, color=[0.0,1.0,0.0], length = 0.05,width=2.0):
         length (float, optional): The length of the lines.  Defaults to 0.5.
         width (float, optional): The width of the lines. Defaults to 2.0.
     """
-    
-    direction = np.array([1,0,0])
-    p.addUserDebugLine(point + length*direction,point - length*direction, 
+
+    direction = np.array([1, 0, 0])
+    p.addUserDebugLine(point + length*direction, point - length*direction,
                        lineColorRGB=color, lineWidth=width, lifeTime=0)
 
-    direction = np.array([0,1,0])
-    p.addUserDebugLine(point + length*direction,point - length*direction, 
+    direction = np.array([0, 1, 0])
+    p.addUserDebugLine(point + length*direction, point - length*direction,
                        lineColorRGB=color, lineWidth=width, lifeTime=0)
 
-    direction = np.array([0,0,1])
-    p.addUserDebugLine(point + length*direction,point - length*direction, 
+    direction = np.array([0, 0, 1])
+    p.addUserDebugLine(point + length*direction, point - length*direction,
                        lineColorRGB=color, lineWidth=width, lifeTime=0)
-    
 
-def draw_path(path,color=[0.0,1.0,0.0],width=2.0):
+
+def draw_path(path, color=[0.0, 1.0, 0.0], width=2.0):
     """Draws a path in the workspace
 
     Args:
@@ -32,15 +33,15 @@ def draw_path(path,color=[0.0,1.0,0.0],width=2.0):
         color (list, optional): RGB color. Defaults to [0.0,1.0,0.0].
         width (float, optional): The width of the lines. Defaults to 2.0.
     """
-    path_steps =len(path[0])
-    for i in range(1,path_steps):
-        current_point = path[:,i]
-        previous_point = path[:,i-1]
-        p.addUserDebugLine(current_point,previous_point, 
-                       lineColorRGB=color, lineWidth=width, lifeTime=0)
+    path_steps = len(path[0])
+    for i in range(1, path_steps):
+        current_point = path[:, i]
+        previous_point = path[:, i-1]
+        p.addUserDebugLine(current_point, previous_point,
+                           lineColorRGB=color, lineWidth=width, lifeTime=0)
 
 
-def draw_coordinate_system(position,orientation, length = 0.1,width=2.0,life_time=0):
+def draw_coordinate_system(position, orientation, length=0.1, width=2.0, life_time=0):
     """This function draws a coordinate system at a given position
 
     Args:
@@ -58,15 +59,16 @@ def draw_coordinate_system(position,orientation, length = 0.1,width=2.0,life_tim
 
     x_direction = np.array([np.cos(yaw)*np.cos(pitch),
                             np.sin(yaw)*np.cos(pitch),
-                            np.sin(pitch) ])
+                            np.sin(pitch)])
     y_direction = -1*np.array([-np.cos(yaw)*np.sin(pitch)*np.sin(roll)-np.sin(yaw)*np.cos(roll),
-                               -np.sin(yaw)*np.sin(pitch)*np.sin(roll)+np.cos(yaw)*np.cos(roll),
+                               -np.sin(yaw)*np.sin(pitch) *
+                               np.sin(roll)+np.cos(yaw)*np.cos(roll),
                                np.cos(pitch)*np.sin(roll)])
-    z_direction =  np.cross(x_direction,y_direction)
+    z_direction = np.cross(x_direction, y_direction)
 
-    p.addUserDebugLine(position,position+length*x_direction, 
-                       lineColorRGB=[1,0,0], lineWidth=width, lifeTime=life_time)
-    p.addUserDebugLine(position,position+length*y_direction, 
-                       lineColorRGB=[0,1,0], lineWidth=width, lifeTime=life_time)
-    p.addUserDebugLine(position,position+length*z_direction, 
-                       lineColorRGB=[0,0,1], lineWidth=width, lifeTime=life_time)
+    p.addUserDebugLine(position, position+length*x_direction,
+                       lineColorRGB=[1, 0, 0], lineWidth=width, lifeTime=life_time)
+    p.addUserDebugLine(position, position+length*y_direction,
+                       lineColorRGB=[0, 1, 0], lineWidth=width, lifeTime=life_time)
+    p.addUserDebugLine(position, position+length*z_direction,
+                       lineColorRGB=[0, 0, 1], lineWidth=width, lifeTime=life_time)

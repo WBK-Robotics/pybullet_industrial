61b9d140bf39cf13a5011f6e5ccbc1ee69798f61
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-05-18 16:22:43 +0200

    moved mose selection from example to library

--- examples/mouse_selection.py
+++ examples/mouse_selection.py
@@ -1,36 +1,6 @@
 import os
-import time
 import pybullet as p
 import wbk_sim as wbk
-import numpy as np
-
-
-def get_object_id_from_mouse():
-    mouseEvents = p.getMouseEvents()
-    for e in mouseEvents:
-        if ((e[0] == 2) and (e[3] == 0) and (e[4] & p.KEY_WAS_TRIGGERED)):
-            mouseX = e[1]
-            mouseY = e[2]
-            width, height, _, _, _, camera_forward, horizontal, vertical, _, _, dist, camera_target = p.getDebugVisualizerCamera()
-            horizontal = np.array(horizontal)
-            vertical = np.array(vertical)
-            camera_target = np.array(camera_target)
-            camera_forward = np.array(camera_forward)
-
-            camera_position = camera_target-dist*camera_forward #current position of the debug camera
-            ray_start_pos = camera_position
-            far_plane = 10000
-            ray_forward = camera_target-camera_position
-            ray_forward = far_plane*ray_forward/ np.linalg.norm(ray_forward)
-
-            dHor = horizontal/width
-            dVer = vertical/height
-
-            ray_end_pos = ray_start_pos+ ray_forward - 0.5*horizontal+0.5*vertical+float(mouseX)* dHor - float(mouseY)*dVer
-            rayInfo = p.rayTest(ray_start_pos, ray_end_pos )
-            hit = rayInfo[0]
-            return hit[0], hit[1]
-    return -1,-1
 
 
 
@@ -57,7 +27,7 @@ if __name__ == "__main__":
     p.getCameraImage(64, 64, renderer=p.ER_BULLET_HARDWARE_OPENGL)
 
     while (1):
-        objectUid, object_index = get_object_id_from_mouse()
+        objectUid, object_index = wbk.get_object_id_from_mouse()
         if (objectUid >= 0):
             p.changeVisualShape(objectUid, object_index, rgbaColor=colors[currentColor])
             currentColor += 1

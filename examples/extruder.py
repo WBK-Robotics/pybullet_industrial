693f69ee34e9d6299c9472382fc4f616f871e877
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-05-24 09:13:08 +0200

    added collision shape and path so that extruder draws figure

--- examples/extruder.py
+++ examples/extruder.py
@@ -1,8 +1,10 @@
 import os
 import time
 import pybullet as p
+import pybullet_data
 import pybullet_industrial as pi
 import numpy as np
+from lemniscate import build_lemniscate_path
 
 class GlueParticle:
 
@@ -49,27 +51,50 @@ if __name__ == "__main__":
 
     physics_client = p.connect(p.GUI)
     p.setPhysicsEngineParameter(numSolverIterations=5000)
+
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    monastryId =p.createCollisionShape(p.GEOM_MESH,
+                            fileName="samurai_monastry.obj",
+                            flags=p.GEOM_FORCE_CONCAVE_TRIMESH)
+    orn = p.getQuaternionFromEuler([1.5707963, 0, 0])
+    p.createMultiBody(0, monastryId, baseOrientation=orn)
+
     start_orientation = p.getQuaternionFromEuler([0, 0, 0])
+
     robot = pi.RobotBase(urdf_file1, [0, 0, 0], start_orientation)
+
     extruder = pi.EndeffectorTool(
         urdf_file2, [1.9, 0, 1.2], start_orientation)
-    p.setRealTimeSimulation(1)
     extruder.couple(robot, 'link6')
 
-    target_position = np.array([1.9, 0, 1.2])
-    target_orientation = p.getQuaternionFromEuler([-np.pi, 0, 0])
-
     visualShapeId = p.createVisualShape(shapeType=p.GEOM_SPHERE, rgbaColor=[1, 1, 1, 1], radius=0.03)
+
+    target_position = np.array([1.9, 0, 0.3])
+    target_orientation = p.getQuaternionFromEuler([-np.pi, 0, 0])
+    steps = 100
+    test_path = build_lemniscate_path(target_position, steps, 0.3, 0.8)
+    pi.draw_path(test_path)
+    target_orientation = p.getQuaternionFromEuler([0, 0, 0])
+
+    p.setRealTimeSimulation(1)
+    for i in range(20):
+        extruder.set_tool_pose(test_path[:, 0], target_orientation)
+        time.sleep(0.1)
     while True:
-        ray_pos,ray_ori = extruder.get_tool_pose()
-        ray_cast_results = cast_rays(ray_pos,ray_ori,np.pi/3,100,1)
-        for i in range(100):
-            ray_intersection = ray_cast_results[i][3]
-            mb = p.createMultiBody(baseMass=0,
-                           baseCollisionShapeIndex=-1,
-                           baseVisualShapeIndex=visualShapeId,
-                           basePosition=ray_intersection,
-                           useMaximalCoordinates=True)
-        time.sleep(0.01)
-            
+        for i in range(steps):
+            extruder.set_tool_pose(test_path[:, i], target_orientation)
+            position, orientation = extruder.get_tool_pose()
+            pi.draw_coordinate_system(position, orientation)
+
+            ray_cast_results = cast_rays(position, orientation,np.pi/6,20,1)
+            for i in range(20):
+                ray_intersection = ray_cast_results[i][3]
+                mb = p.createMultiBody(baseMass=0,
+                            baseCollisionShapeIndex=-1,
+                            baseVisualShapeIndex=visualShapeId,
+                            basePosition=ray_intersection,
+                            useMaximalCoordinates=True)
+
+            time.sleep(0.005)
+
 

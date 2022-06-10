b2719a5c2ad1b3f5c1617f100fe5a926232aac03
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-06-10 11:29:00 +0200

    deleted trailing whitespace

--- src/pybullet_industrial/extruder.py
+++ src/pybullet_industrial/extruder.py
@@ -22,13 +22,13 @@ class Extruder(EndeffectorTool):
                                        'material properties': {'particle size':0.03,
                                                                'color' : [1, 0, 0, 1]},
                                        'maximum distance':1,'material':None,
-            coupled_robot ([type], optional): A wbk_sim.Robot object if 
-                                              the robot is coupled from the start. 
+            coupled_robot ([type], optional): A wbk_sim.Robot object if
+                                              the robot is coupled from the start.
                                               Defaults to None.
-            tcp_frame ([type], optional): The name of the urdf_link 
+            tcp_frame ([type], optional): The name of the urdf_link
                                           describing the tool center point.
                                           Defaults to None in which case the last link is used.
-            connector_frame ([type], optional): The name of the urdf_link 
+            connector_frame ([type], optional): The name of the urdf_link
                                                 at which a robot connects.
                                                 Defaults to None in which case the base link is used.
 
@@ -52,7 +52,7 @@ class Extruder(EndeffectorTool):
         """Extrudes material from the specified tcp_frame.
 
         Args:
-            tcp_frame (str, optional): the name of the link from which to extrude the material. 
+            tcp_frame (str, optional): the name of the link from which to extrude the material.
                                        Defaults to None in which case the default tcp is used
         """
         position, orientation = self.get_tool_pose(tcp_frame)

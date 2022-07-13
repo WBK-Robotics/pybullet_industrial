renamed back to circular interpolation and fixed angle calculation

examples/path_creation.py
@@ -16,10 +16,7 @@ def build_circular_path(center, radius, min_angle, max_angle, step_num):
     Returns:
         array: array of 3 dimensional path points
     """
-    if min_angle <= 0:
-        min_angle += 2*np.pi
-    if max_angle <= 0:
-        max_angle += 2*np.pi
+
     circular_path = np.zeros((3, step_num))
     circular_path[2, :] = center[2]
     for j in range(step_num):
@@ -36,14 +33,12 @@ def linear_interpolation(start_point, end_point, samples):
 
 
 def circular_interpolation(start_point, end_point, radius, samples, clockwise=True):
-    connecting_line = end_point-start_point
+    planar_end_point = np.array([end_point[0], end_point[1], start_point[2]])
+    connecting_line = planar_end_point-start_point
     distance_between_points = np.linalg.norm(connecting_line)
     if radius <= distance_between_points/2:
         raise ValueError("The radius needs to be at least " +
                          str(distance_between_points/2))
-    if start_point[2] != end_point[2]:
-        raise ValueError("For circular interpolation both points need to have the same height." +
-                         " For height differences use helical interpolation")
 
     center_distance_from_connecting_line = np.sqrt(
         radius**2-distance_between_points**2/4)
@@ -57,13 +52,14 @@ def circular_interpolation(start_point, end_point, radius, samples, clockwise=Tr
 
     circle_center = start_point+connecting_line/2+center_distance_from_connecting_line * \
         orthogonal_vector/np.linalg.norm(orthogonal_vector)
-    print(circle_center, start_point+connecting_line/2, orthogonal_vector /
-          np.linalg.norm(orthogonal_vector), distance_between_points, center_distance_from_connecting_line)
 
     angle_range = np.arccos(center_distance_from_connecting_line/radius)*2
     initial_angle = np.arctan2(
         start_point[1]-circle_center[1], start_point[0]-circle_center[0])
-    return build_circular_path(circle_center, radius, initial_angle, initial_angle+angle_range, samples)
+    planar_path = build_circular_path(
+        circle_center, radius, initial_angle, initial_angle+angle_range, samples)
+    planar_path[2] = np.linspace(start_point[2], end_point[2], samples)
+    return planar_path
 
 
 if __name__ == "__main__":
@@ -89,6 +85,11 @@ if __name__ == "__main__":
     test_path = circular_interpolation(
         np.array([0, -1, 0]), np.array([1, 0, 0]), 1, 50)
     pi.draw_path(test_path, color=[0, 1, 0])
+
+    # -- quadrant
+    test_path = circular_interpolation(
+        np.array([0, -1, 0]), np.array([-1, 0, 0.5]), 1, 50)
+    pi.draw_path(test_path, color=[0, 0, 1])
 
     p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
 
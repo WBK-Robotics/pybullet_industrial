0b0eb29fba9fa4e403bc27a6b691cb80ed21abda
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-07-15 13:50:46 +0200

    added cubic spline interpolation function

--- examples/path_creation.py
+++ examples/path_creation.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import pybullet_industrial as pi
 import numpy as np
+import scipy.interpolate as sci
 
 
 def build_circular_path(center, radius, min_angle, max_angle, step_num, clockwise=True):
@@ -81,6 +82,23 @@ def circular_interpolation(start_point, end_point, radius, samples, axis=2, cloc
     return path
 
 
+def spline_interpolation(points, samples):
+    s = np.linspace(0, 1, len(points[0]))
+
+    path = np.zeros((3, samples))
+    print(points[0], s)
+    cs_x = sci.CubicSpline(s, points[0])
+    cs_y = sci.CubicSpline(s, points[1])
+    cs_z = sci.CubicSpline(s, points[2])
+
+    cs_s = np.linspace(0, 1, samples)
+    path[0] = cs_x(cs_s)
+    path[1] = cs_y(cs_s)
+    path[2] = cs_z(cs_s)
+
+    return path
+
+
 if __name__ == "__main__":
 
     p.connect(p.GUI)
@@ -121,6 +139,10 @@ if __name__ == "__main__":
     test_path = circular_interpolation(
         np.array([0, 0, 0]), np.array([1, 0, 1]), 1, 50, axis=1)
     pi.draw_path(test_path, color=[1, 1, 0])
+
+    test_path = spline_interpolation(
+        [[0, 1, 0], [0, 0, 1], [0, 1, 1]], samples=50)
+    pi.draw_path(test_path, color=[1, 0.5, 0.5])
 
     p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
 

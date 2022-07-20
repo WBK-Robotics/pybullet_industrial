a78e4be4ee1dd9a746b2540efe275161f40980be
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-07-20 15:46:51 +0200

    added constraints to fix pendulum base in place

--- examples/tcp_force.py
+++ examples/tcp_force.py
@@ -6,18 +6,29 @@ import pybullet_data
 if __name__ == "__main__":
 
     physics_client = p.connect(p.GUI)
-    p.setGravity(0, 0, -10)
+    #p.setGravity(0, 0, -10)
 
     urdf_path = os.path.join(
         pybullet_data.getDataPath(), "cartpole.urdf")
 
-    p.setPhysicsEngineParameter(numSolverIterations=5000)
     start_orientation = p.getQuaternionFromEuler([0, 0, 0])
 
     pendulum = pi.EndeffectorTool(
-        urdf_path, [1.9, 0, 1.2], start_orientation)
+        urdf_path, [0, 0, 0], start_orientation)
 
-    p.setTimeStep(0.1)
+    p.createConstraint(pendulum.urdf,
+                       -1, pendulum.urdf, 0,
+                       p.JOINT_FIXED,
+                       [0, 0, 0],
+                       [0, 0, 0],
+                       [0, 0, 0],
+                       None,
+                       [0, 0, 0])
+
+    p.resetJointState(pendulum.urdf, 1, targetValue=0.5)
+    p.setTimeStep(1)
+    pi.draw_robot_frames(pendulum, life_time=0)
     while True:
-        pendulum.apply_tcp_force([0, 0, -10])
+        pendulum.apply_tcp_force([0, 0, 10])
+        position, orientation = pendulum.get_tool_pose()
         p.stepSimulation()

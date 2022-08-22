94b532cb1e0673278a455c5eb16696657077f9a9
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-08-22 10:07:44 +0200

    started simple example for cutting speed simulation

--- examples/milling_force_model.py
+++ examples/milling_force_model.py
@@ -1,188 +1,15 @@
+import os
+
+import numpy as np
 import pybullet as p
 import pybullet_industrial as pi
-import os
-import numpy as np
-from typing import Dict
-
-
-class MillingTool(pi.EndeffectorTool):
-
-    def __init__(self, urdf_model: str, start_position: np.array, start_orientation: np.array,
-                 raycast_properties: Dict, coupled_robot: pi.RobotBase = None,
-                 tcp_frame: str = None, connector_frame: str = None):
-        """Initializes the milling tool.
-
-        Args:
-            urdf_model (str): The path to the urdf model of the milling tool
-            start_position (np.array): The position of the tool center point
-            start_orientation (np.array): The orientation of the tool center point
-            raycast_properties (Dict): A dictionary containing the properties of the milling tool.
-                                       Default values are:
-                                       'diameter':0.05, 'height':0.01,
-                                       'number of rays':10,'rotation speed':0.1, 'number of teeth':5
-            coupled_robot (RobotBase, optional): A pybullet_industrial.RobotBase object if
-                                                 the robot is coupled from the start.
-                                                 Defaults to None.
-            tcp_frame (str, optional): The name of the urdf_link
-                                       describing the tool center point.
-                                       Defaults to None in which case the last link is used.
-            connector_frame (str, optional): The name of the urdf_link
-                                             at which a robot connects.
-                                             Defaults to None in which case the base link is used
-
-        """
-        super().__init__(urdf_model, start_position, start_orientation,
-                         coupled_robot, tcp_frame, connector_frame)
-
-        self.properties = {'diameter': 0.05,
-                           'rotation speed': 1,
-                           'number of teeth': 5,
-                           'height': 0.1,
-                           'number of rays': 10}
-        self.current_angle = 0
-        if raycast_properties is not None:
-            self.change_properties(raycast_properties)
-
-    def get_cutting_state(self, ray_cast_result, tcp_frame=None):
-        """A helpfer function calculating the cutting depth and speed of the tool.
-
-        Args:
-            ray_cast_result (list): The result of a batch ray cast as performed
-                                      by the mill function
-            tcp_frame (str, optional): The tool center point frame name. Defaults to None.
-
-        Returns:
-            float: the speed at which the cutting tool is moved into the material
-            (np.array): an array of the depth of the cutting tool at each tooth
-        """
-        if tcp_frame is None:
-            tcp_id = self._tcp_id
-        else:
-            tcp_id = self._convert_link_to_id(tcp_frame)
-
-        link_state = p.getLinkState(self.urdf, tcp_id, computeLinkVelocity=1)
-        cutting_speed = np.linalg.norm(link_state[7])
-
-        ray_cast_per_teeth = [ray_cast_result[i:i+self.properties['number of rays']]
-                              for i in range(0, len(ray_cast_result),
-                                             self.properties['number of rays'])]
-
-        # calculate the cutting depth based on the amount of rays that are hitting a body
-        # if the cutting depth is zero, the tool is not cutting
-        cutting_depth = [sum([ray_cast[0] != -1 for ray_cast in ray_cast_per_teeth[i]])
-                         for i in range(self.properties['number of teeth'])]
-        cutting_depth = np.array(
-            cutting_depth)*self.properties['height']/self.properties['number of rays']
-
-        return cutting_speed, cutting_depth
-
-    def cast_rays(self, position: np.array, orientation: np.array):
-        """Randomly casts rays withn a given range from a specified position and orientation
-
-        Args:
-            position (np.array): start position of the raycast
-            orientation (np.array): start orientation of the raycast
-
-        Returns:
-            List: The result of a raycast
-        """
-        ray_start_pos = []
-        ray_end_pos = []
-        angle_between_teeth = 2*np.pi/self.properties['number of teeth']
-
-        rot_matrix = p.getMatrixFromQuaternion(orientation)
-        rot_matrix = np.array(rot_matrix).reshape(3, 3)
-
-        for i in range(self.properties['number of teeth']):
-            end_point = self.properties['diameter']*np.array(
-                [np.sin(i*angle_between_teeth+self.current_angle),
-                 np.cos(i*angle_between_teeth+self.current_angle),
-                 0])
-            for j in range(self.properties['number of rays']):
-                height_adjustment = np.array(
-                    [0, 0, j*self.properties['height']/self.properties['number of rays']])
-
-                start_position = position+rot_matrix@height_adjustment
-                end_position = position + \
-                    rot_matrix@(end_point+height_adjustment)
-
-                p.addUserDebugLine(start_position, end_position,
-                                   [1, 0, 0], 1, lifeTime=1)
-                ray_start_pos.append(start_position)
-                ray_end_pos.append(end_position)
-
-        ray_cast_results = p.rayTestBatch(ray_start_pos, ray_end_pos)
-
-        return ray_cast_results
-
-    def mill(self, tcp_frame=None):
-        """Function that performs a milling operation.
-
-        Args:
-            tcp_frame (str, optional): The name of the tool center point frame. Defaults to None.
-
-        Returns:
-            list: A list containing the ids of the bodies that were removed
-        """
-
-        position, orientation = self.get_tool_pose(tcp_frame)
-        ray_cast_results = self.cast_rays(position, orientation)
-
-        cutting_speed, cutting_depth = self.get_cutting_state(
-            ray_cast_results, tcp_frame)
-        cutting_force = self.force_model(cutting_speed,
-                                         cutting_depth,
-                                         self.properties['diameter'],
-                                         self.properties['rotation speed'])
-        self.apply_tcp_force(cutting_force, tcp_frame)
-
-        self.current_angle = self.current_angle + \
-            self.properties['rotation speed']
-
-        removed_objects = []
-        for ray_intersection in ray_cast_results:
-            if ray_intersection[0] != -1:
-                p.removeBody(ray_intersection[0])
-                removed_objects.append(ray_intersection[0])
-        return removed_objects
-
-    @staticmethod
-    def force_model(cutting_speed, cutting_depth, diameter, rotation_speed):
-        """A force model that is used to calculate the force that is applied to the tool.
-
-        Args:
-            cutting_speed (float): the speed at which the cutting tool is moved into the material
-            cutting_depth (np.array): an array of the depth of the cutting tool at each tooth
-            diameter (float): the diameter of the cutting tool
-            rotation_speed (float): the speed at which the cutting tool rotates
-
-        Returns:
-            np.array: an array of the force that is applied to the cutting tool
-        """
-        force = np.zeros(3)
-        return force
-
-    def change_properties(self, new_properties: Dict):
-        """Allows retroactive changes to the ray casting properties.
-
-        Args:
-            new_properties (Dict): A dictionary containing values and keys that
-                                   should be changed
-        Raises:
-            KeyError: If a key is not a valid property
-        """
-        for key in new_properties:
-            if not key in self.properties:
-                raise KeyError("The specified property keys are not valid" +
-                               " Valid keys are: "+str(self.properties.keys()))
-            self.properties[key] = new_properties[key]
-
 
 if __name__ == "__main__":
     dirname = os.path.dirname(__file__)
     urdf_file = os.path.join(dirname,
                              'robot_descriptions', 'milling_head.urdf')
-
+    urdf_file1 = os.path.join(dirname,
+                              'robot_descriptions', 'comau_NJ290_3-0_m.urdf')
     cid = p.connect(p.GUI)
     p.setPhysicsEngineParameter(numSolverIterations=5000)
 
@@ -190,16 +17,28 @@ if __name__ == "__main__":
                           'rotation speed': 2*np.pi/5,
                           'number of teeth': 5,
                           'height': 0.1,
-                          'number of rays': 10}
-
+                          'number of rays': 1}
+    robot = pi.RobotBase(urdf_file1, [0, 0, 0], [0, 0, 0, 1])
     position = [0.00, 0.0, 0.6]
-    milling_tool = MillingTool(
+    milling_tool = pi.MillingTool(
         urdf_file, position, [0, 0, 0, 1], remover_properties)
     milling_tool.set_tool_pose(position, [0, 0, 0, 1])
+    milling_tool.couple(robot, 'link6')
 
-    p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
-    while (1):
-        pi.Plastic([0, 0, 0, position+np.array([0., 0.05, 0])],
-                   {'particle size': 0.02, 'color': [1, 0, 0, 1]})
-        results = milling_tool.mill()
+    test_path = pi.linear_interpolation(
+        [2.3, 0, 2.2], [2.3, 0, 0.2], 50)
+
+    for _ in range(500):
+        milling_tool.set_tool_pose([2.3, 0, 2.2], [0, 0, 0, 1])
         p.stepSimulation()
+
+    for steps in test_path:
+        milling_tool.set_tool_pose(steps[0], [0, 0, 0, 1])
+        print(steps[0])
+        p.stepSimulation()
+        tool_position, tool_orientation = milling_tool.get_tool_pose()
+        ray_cast_results = milling_tool.cast_rays(
+            tool_position, tool_orientation)
+        cutting_speed, _ = milling_tool.get_cutting_state(
+            ray_cast_results)
+        print(cutting_speed)

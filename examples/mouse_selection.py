38d96937c7566e9501d0f102da330b47af946f3e
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2022-08-29 09:27:11 +0200

    cleaned up mouse selection simulation, switched 2nd robot type to kuka

--- examples/mouse_selection.py
+++ examples/mouse_selection.py
@@ -1,24 +1,24 @@
 import os
+
+import numpy as np
 import pybullet as p
 import pybullet_industrial as pi
-
 
 if __name__ == "__main__":
     dirname = os.path.dirname(__file__)
     urdf_file1 = os.path.join(
         dirname, 'robot_descriptions', 'comau_nj290_robot.urdf')
+    urdf_file2 = os.path.join(
+        dirname, 'robot_descriptions', 'kuka_robot.urdf')
 
     physics_client = p.connect(p.GUI)
     p.setPhysicsEngineParameter(numSolverIterations=1000)
     start_orientation = p.getQuaternionFromEuler([0, 0, 0])
-    robot = pi.RobotBase(urdf_file1, [0, 0, 0], start_orientation, 'link6')
-    robot = pi.RobotBase(urdf_file1, [1, 0, 0], start_orientation, 'link6')
+    robot = pi.RobotBase(urdf_file1, [-2, 0, 0], start_orientation)
+    start_orientation = p.getQuaternionFromEuler([0, 0, np.pi])
+    robot = pi.RobotBase(urdf_file2, [2, 0, 0], start_orientation)
 
     p.setRealTimeSimulation(1)
-
-    target_position = [1.9, 0, 1.2]
-    drawing_position = [2.4, 0, 1.2]
-    pi.draw_point(target_position, length=0.05)
 
     colors = [[1, 0, 0, 1], [0, 1, 0, 1], [0, 0, 1, 1], [1, 1, 1, 1]]
     currentColor = 0

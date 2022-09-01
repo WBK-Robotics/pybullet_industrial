354fd087af61ae98a136c2dbebe4d823a127dceb
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-09-01 10:08:55 +0200

    started test for force direction and magnitude

--- examples/milling_force_model.py
+++ examples/milling_force_model.py
@@ -8,36 +8,59 @@ if __name__ == "__main__":
     dirname = os.path.dirname(__file__)
     urdf_file = os.path.join(dirname,
                              'robot_descriptions', 'milling_head.urdf')
-    urdf_file1 = os.path.join(dirname,
-                              'robot_descriptions', 'comau_nj290_robot.urdf')
+
     cid = p.connect(p.GUI)
     p.setPhysicsEngineParameter(numSolverIterations=5000)
 
     remover_properties = {'diameter': 0.1,
                           'rotation speed': 2*np.pi/5,
-                          'number of teeth': 5,
+                          'number of teeth': 6,
                           'height': 0.1,
-                          'number of rays': 1}
-    robot = pi.RobotBase(urdf_file1, [0, 0, 0], [0, 0, 0, 1])
-    position = [0.00, 0.0, 0.6]
+                          'number of rays': 10}
+
+    position = [0.00, 0.0, 0.0]
     milling_tool = pi.MillingTool(
         urdf_file, position, [0, 0, 0, 1], remover_properties)
     milling_tool.set_tool_pose(position, [0, 0, 0, 1])
-    milling_tool.couple(robot, 'link6')
 
-    test_path = pi.linear_interpolation(
-        [2.3, -0.6, 1.2], [2.3, 0.6, 1.2], 50)
+    pi.MetalVoxel([0, 0, 0, position+np.array([0.0, 0.06, 0])],
+                  {'particle size': 0.1, 'color': [1, 0, 0, 1]})
+    # pi.Plastic([0, 0, 0, position+np.array([0.05, 0., 0])],
+    #           {'particle size': 0.03, 'color': [1, 0, 0, 1]})
 
-    for _ in range(500):
-        milling_tool.set_tool_pose(*test_path.get_start_pose())
+    for _ in range(100):
         p.stepSimulation()
+    tool_position, tool_orientation = milling_tool.get_tool_pose()
 
-    for steps in test_path:
-        milling_tool.set_tool_pose(steps[0], [0, 0, 0, 1])
-        p.stepSimulation()
-        tool_position, tool_orientation = milling_tool.get_tool_pose()
+    force_x = []
+    for i in range(10):
         ray_cast_results = milling_tool.cast_rays(
             tool_position, tool_orientation)
-        cutting_speed, _ = milling_tool.get_cutting_state(
+        cutting_speed, cutting_depth = milling_tool.get_cutting_state(
             ray_cast_results)
-        print(cutting_speed)
+
+        teeth_angles = [i * 2*np.pi/milling_tool.properties['number of teeth'] +
+                        milling_tool.current_angle for i in range(milling_tool.properties['number of teeth'])]
+
+        cutting_force = milling_tool.force_model(0.01,
+                                                 cutting_depth,
+                                                 milling_tool.properties['diameter'],
+                                                 milling_tool.properties['rotation speed'],
+                                                 milling_tool.properties['material_specific_force'],
+                                                 milling_tool.properties['chip_thickness_exponent'],
+                                                 teeth_angles)
+        # print(cutting_force)
+        ray_cast_results = milling_tool.cast_rays(
+            tool_position, tool_orientation)
+
+        milling_tool.current_angle += np.pi*2/5*0.1
+
+        print(cutting_force[0], cutting_force[1])
+        force_x.append(cutting_force[0])
+        p.stepSimulation()
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(force_x)
+    print(np.mean(force_x))
+    plt.show()

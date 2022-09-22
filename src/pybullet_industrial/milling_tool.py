corrected calculation of feed per tooth
To calculate the feed per tooth, one has to divide by the number of teeth, not the diameter. 
When you divide the feed speed by the rotation speed you get the distance the tool travels in one rotation. This distance equals the length of the material to be removed, a.k.a the feed. When you divide this by the number of teeth you get the feed per tooth.

src/pybullet_industrial/milling_tool.py
@@ -180,7 +180,7 @@ class MillingTool(pi.EndeffectorTool):
             np.array: an array of the force that is applied to the cutting tool at the tcp
         """
         h = cutting_speed / \
-            (self.properties['rotation speed'] * self.properties['diameter'])
+            (self.properties['rotation speed'] * self.properties['number of teeth'])
         k_c = self.properties['material_specific_force'] / \
             (h ** self.properties['chip_thickness_exponent'])
 
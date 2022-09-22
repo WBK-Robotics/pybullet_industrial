5641a140748e805ef7777ea144258d0340ca78b2
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-09-22 13:27:18 +0200

    adjusted colors and added header logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,9 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
+html_theme_options = {
+    'style_nav_header_background': '#009682', 'logo_only': True}
+html_logo = "header_logo.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

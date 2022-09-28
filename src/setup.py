fd01de8cbe2ea5d15c3b3ea85087e3032ab141e9
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-09-28 10:20:59 +0200

    added long desscription for pypi release

--- src/setup.py
+++ src/setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
     version='1.0',
     author='Jan Baumgärtner, Malte Hansjosten, Dominik Schönhofen',
     description='Pybullet_industrial is a process-aware robot simulation framework for pybullet.',
+    long_description='Pybullet_industrial is a process-aware robot simulation framework for pybullet.',
     url='https://pybullet-industrial.readthedocs.io/en/latest/',
     license='MIT',
     install_requires=['numpy', 'pybullet', 'scipy'],

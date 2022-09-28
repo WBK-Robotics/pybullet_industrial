da428ecc7c143f60e3160ea13b9067cffe3eb3c0
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-09-28 10:13:57 +0200

    folded requirements into setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,18 +2,15 @@
 
 import setuptools
 
-with open("requirements.txt", "r") as fh:
-    requirements = fh.readlines()
-
 
 setuptools.setup(
     name='pybullet_industrial',
-    version='0.9',
+    version='1.0',
     author='Jan Baumgärtner, Malte Hansjosten, Dominik Schönhofen',
     description='Pybullet_industrial is a process-aware robot simulation framework for pybullet.',
     url='https://pybullet-industrial.readthedocs.io/en/latest/',
     license='MIT',
-    install_requires=[req for req in requirements if req[:2] != "# "],
+    install_requires=['numpy', 'pybullet', 'scipy'],
     packages=setuptools.find_packages(),
 
 )

37d9c7f5d0a6e963e8411736dca2c9e93a87bd55
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2023-12-21 10:43:49 +0100

    created abstract template class for other agv kinematic types

--- examples/agv_robot.py
+++ examples/agv_robot.py
@@ -4,27 +4,11 @@ import pybullet as p
 
 
 
+#Template Class for all AGV Robots
+class AGVRobot:
 
-class DiffDriveAGV:
-
-
-    def __init__(self, urdf_model: str, start_position: np.array, start_orientation: np.array,
-                 left_wheel_name: str, right_wheel_name: str,diff_drive_params: dict,
+    def __init__(self,urdf_model: str, start_position: np.array, start_orientation: np.array,
                  position_controller=None):
-        """Initializes the robot.
-
-        Args:
-            urdf_model (str): The path to the URDF file of the robot.
-            start_position (np.array): The start position of the robot.
-            start_orientation (np.array): The start orientation of the robot.
-            left_wheel_name (str): The name of the left wheel joint.
-            right_wheel_name (str): The name of the right wheel joint.
-            diff_drive_params (dict): A dictionary containing the parameters of the robot.
-            position_controller (function, optional): A function that calculates the wheel commands
-                                                      for a given distance and angle to the target.
-                                                      Defaults to None. In this case the standard
-                                                      position controller is used.
-        """
 
         urdf_flags = p.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS
         self.urdf = p.loadURDF(urdf_model,
@@ -37,85 +21,10 @@ class DiffDriveAGV:
         else:
             self.position_controller = position_controller
 
-        num_joints = p.getNumJoints(self.urdf)
-        self.left_wheel_index = None
-        self.right_wheel_index = None
-
-        for joint_index in range(num_joints):
-            joint_info = p.getJointInfo(self.urdf, joint_index)
-            joint_name = joint_info[1].decode('UTF-8')
-
-            if joint_name == left_wheel_name:
-                self.left_wheel_index = joint_index
-            elif joint_name == right_wheel_name:
-                self.right_wheel_index = joint_index
-
-        if self.left_wheel_index is None or self.right_wheel_index is None:
-            raise ValueError("Wheel names not found in URDF.")
-
-
-        self.target_pose = np.zeros(3)
-
-        self.wheel_radius = diff_drive_params["wheel_radius"]
-        self.track_width = diff_drive_params["track_width"]
-        self.max_linear_velocity = diff_drive_params["max_linear_velocity"]
-        self.max_angular_velocity = diff_drive_params["max_angular_velocity"]
-
-
-        # set the control mode of the wheels to VELOCITY_CONTROL
-        p.setJointMotorControl2(self.urdf, self.left_wheel_index, p.VELOCITY_CONTROL)
-        p.setJointMotorControl2(self.urdf, self.right_wheel_index, p.VELOCITY_CONTROL)
 
 
 
-
-
-
-
-    def __calculate_wheel_comands(self, linear_velocity: float, angular_velocity: float):
-        """Calculates the wheel commands for a differential drive robot.
-
-        Args:
-            linear_velocity (float): The linear velocity of the robot.
-            angular_velocity (float): The angular velocity of the robot.
-
-        Returns:
-            np.array: The wheel commands.
-        """
-        turning_contribution = angular_velocity * self.track_width / (2*self.wheel_radius)
-        driving_contribution = linear_velocity / self.wheel_radius
-        left_wheel_velocity = driving_contribution - turning_contribution
-        right_wheel_velocity = driving_contribution + turning_contribution
-
-
-        return np.array([left_wheel_velocity, right_wheel_velocity])
-
-    def set_velocity(self, linear_velocity: float, angular_velocity: float):
-        """Sets the velocity of the robot.
-
-        Args:
-            linear_velocity (float): The linear velocity of the robot.
-            angular_velocity (float): The angular velocity of the robot.
-        """
-        #clip the velocities to the maximum velocities
-        linear_velocity = np.clip(linear_velocity,
-                                    -self.max_linear_velocity,
-                                    self.max_linear_velocity)
-        angular_velocity = np.clip(angular_velocity,
-                                    -self.max_angular_velocity,
-                                    self.max_angular_velocity)
-
-        wheel_commands = self.__calculate_wheel_comands(linear_velocity, angular_velocity)
-        p.setJointMotorControl2(self.urdf,
-                                self.left_wheel_index,
-                                p.VELOCITY_CONTROL,
-                                targetVelocity=wheel_commands[0])
-        p.setJointMotorControl2(self.urdf,
-                                self.right_wheel_index,
-                                p.VELOCITY_CONTROL,
-                                targetVelocity=wheel_commands[1])
-
-    def __calculate_position_error(self):
+    def _calculate_position_error(self):
         """Calculates the position error of the robot in polar coordinates.
 
         Returns:
@@ -150,51 +59,19 @@ class DiffDriveAGV:
 
         return distance_to_target, angle_to_target, target_angle_error
 
-
     def standard_position_controller(self,distance,angle,target_angle_error):
-        print(distance,angle,target_angle_error)
-        kp_lin=1
-        kp_ang= 0.8
-        kp_target_ang = 0.4
+        pass
 
-        distance_precision = 0.05
-        angle_precision = 0.01
-
-        linear_velocity = np.clip(kp_lin*distance,
-                                  -self.max_linear_velocity,
-                                  self.max_linear_velocity)
-        
-        
-        
-
-        # scale linear velocity so that it it becomes smaller the larger the angle is
-        linear_velocity *= np.clip(1 - 2*np.abs(angle)/np.pi, -1, 1)
-
-        angular_velocity = -1*kp_ang*angle
-        # scala angular velocity so that it becomes smaller
-        # with decreasing distance to avoid singularity
-        #angular_velocity *= np.clip(10*(distance-1), 0, 1)
-
-        # add contribution based on target_angle_error
-        angular_velocity +=  -1*kp_target_ang*target_angle_error
-        angular_velocity = np.clip(angular_velocity,
-                                      -self.max_angular_velocity,
-                                      self.max_angular_velocity)
-        
-        if (distance < distance_precision) and (np.abs(target_angle_error) < angle_precision):
-            linear_velocity = 0
-            angular_velocity = 0
-
-        print(linear_velocity,angular_velocity)
-        return linear_velocity, angular_velocity
+    def set_velocity(self,velocity_vector):
+        pass
 
     def update_position_loop(self):
         """Updates the position of the robot in a loop.
         """
 
-        distance, angle, target_angle_error = self.__calculate_position_error()
-        linear_velocity, angular_velocity = self.position_controller(distance,angle,target_angle_error)
-        self.set_velocity(linear_velocity, angular_velocity)
+        distance, angle, target_angle_error = self._calculate_position_error()
+        velocity_vector = self.position_controller(distance,angle,target_angle_error)
+        self.set_velocity(velocity_vector)
 
     def set_target_pose(self, target_position: np.array,target_orientation: np.array):
         """Sets the target posisition of the robot and executes as velocity command to reach it.
@@ -235,6 +112,150 @@ class DiffDriveAGV:
 
 
 
+class DiffDriveAGV(AGVRobot):
+
+
+    def __init__(self, urdf_model: str, start_position: np.array, start_orientation: np.array,
+                 left_wheel_name: str, right_wheel_name: str,diff_drive_params: dict,
+                 position_controller=None):
+        """Initializes the robot.
+
+        Args:
+            urdf_model (str): The path to the URDF file of the robot.
+            start_position (np.array): The start position of the robot.
+            start_orientation (np.array): The start orientation of the robot.
+            left_wheel_name (str): The name of the left wheel joint.
+            right_wheel_name (str): The name of the right wheel joint.
+            diff_drive_params (dict): A dictionary containing the parameters of the robot.
+            position_controller (function, optional): A function that calculates the wheel commands
+                                                      for a given distance and angle to the target.
+                                                      Defaults to None. In this case the standard
+                                                      position controller is used.
+        """
+
+        super().__init__(urdf_model, start_position, start_orientation, position_controller)
+
+        num_joints = p.getNumJoints(self.urdf)
+        self.left_wheel_index = None
+        self.right_wheel_index = None
+
+        for joint_index in range(num_joints):
+            joint_info = p.getJointInfo(self.urdf, joint_index)
+            joint_name = joint_info[1].decode('UTF-8')
+
+            if joint_name == left_wheel_name:
+                self.left_wheel_index = joint_index
+            elif joint_name == right_wheel_name:
+                self.right_wheel_index = joint_index
+
+        if self.left_wheel_index is None or self.right_wheel_index is None:
+            raise ValueError("Wheel names not found in URDF.")
+
+
+        self.target_pose = np.zeros(3)
+
+        self.wheel_radius = diff_drive_params["wheel_radius"]
+        self.track_width = diff_drive_params["track_width"]
+        self.max_linear_velocity = diff_drive_params["max_linear_velocity"]
+        self.max_angular_velocity = diff_drive_params["max_angular_velocity"]
+
+
+        # set the control mode of the wheels to VELOCITY_CONTROL
+        p.setJointMotorControl2(self.urdf, self.left_wheel_index, p.VELOCITY_CONTROL)
+        p.setJointMotorControl2(self.urdf, self.right_wheel_index, p.VELOCITY_CONTROL)
+
+
+
+
+
+
+    def __calculate_wheel_comands(self, linear_velocity: float, angular_velocity: float):
+        """Calculates the wheel commands for a differential drive robot.
+
+        Args:
+            linear_velocity (float): The linear velocity of the robot.
+            angular_velocity (float): The angular velocity of the robot.
+
+        Returns:
+            np.array: The wheel commands.
+        """
+        turning_contribution = angular_velocity * self.track_width / (2*self.wheel_radius)
+        driving_contribution = linear_velocity / self.wheel_radius
+        left_wheel_velocity = driving_contribution - turning_contribution
+        right_wheel_velocity = driving_contribution + turning_contribution
+
+
+        return np.array([left_wheel_velocity, right_wheel_velocity])
+
+    def set_velocity(self, velocity_vector):
+        """Sets the velocity of the robot.
+
+        Args:
+            velocity_vector (np.array): The velocity vector of the robot.
+        """
+        linear_velocity = velocity_vector[0]
+        angular_velocity = velocity_vector[1]
+
+        #clip the velocities to the maximum velocities
+        linear_velocity = np.clip(linear_velocity,
+                                    -self.max_linear_velocity,
+                                    self.max_linear_velocity)
+        angular_velocity = np.clip(angular_velocity,
+                                    -self.max_angular_velocity,
+                                    self.max_angular_velocity)
+
+        wheel_commands = self.__calculate_wheel_comands(linear_velocity, angular_velocity)
+        p.setJointMotorControl2(self.urdf,
+                                self.left_wheel_index,
+                                p.VELOCITY_CONTROL,
+                                targetVelocity=wheel_commands[0])
+        p.setJointMotorControl2(self.urdf,
+                                self.right_wheel_index,
+                                p.VELOCITY_CONTROL,
+                                targetVelocity=wheel_commands[1])
+
+
+    def standard_position_controller(self,distance,angle,target_angle_error):
+        print(distance,angle,target_angle_error)
+        kp_lin=1
+        kp_ang= 0.8
+        kp_target_ang = 0.4
+
+        distance_precision = 0.05
+        angle_precision = 0.01
+
+        linear_velocity = np.clip(kp_lin*distance,
+                                  -self.max_linear_velocity,
+                                  self.max_linear_velocity)
+
+
+
+
+        # scale linear velocity so that it it becomes smaller the larger the angle is
+        linear_velocity *= np.clip(1 - 2*np.abs(angle)/np.pi, -1, 1)
+
+        angular_velocity = -1*kp_ang*angle
+        # scala angular velocity so that it becomes smaller
+        # with decreasing distance to avoid singularity
+        #angular_velocity *= np.clip(10*(distance-1), 0, 1)
+
+        # add contribution based on target_angle_error
+        angular_velocity +=  -1*kp_target_ang*target_angle_error
+        angular_velocity = np.clip(angular_velocity,
+                                      -self.max_angular_velocity,
+                                      self.max_angular_velocity)
+
+        if (distance < distance_precision) and (np.abs(target_angle_error) < angle_precision):
+            linear_velocity = 0
+            angular_velocity = 0
+
+        return [linear_velocity, angular_velocity]
+
+
+
+
+
+
 
 if __name__ == "__main__":
     import pybullet_industrial as pi
@@ -249,7 +270,7 @@ if __name__ == "__main__":
         linear_velocity = kp_lin*distance,
         angular_velocity = -1*kp_ang*angle,
 
-        return linear_velocity[0], angular_velocity[0]
+        return [linear_velocity[0], angular_velocity[0]]
 
 
     pysics_client = p.connect(p.GUI, options='--background_color_red=1 ' +

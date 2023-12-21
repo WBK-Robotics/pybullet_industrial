c0e65e4fc839fd8075cc7b58470677d54fca1cdb
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2023-12-21 15:49:34 +0100

    implement test class for differential drive robot

--- examples/agv_robot.py
+++ examples/agv_robot.py
@@ -3,6 +3,7 @@ import numpy as np
 import pybullet as p
 import unittest
 import os
+import pybullet_data
 
 #Template Class for all AGV Robots
 class AGVRobot:
@@ -106,8 +107,6 @@ class AGVRobot:
         """
         return p.getBasePositionAndOrientation(self.urdf)
 
-
-
 class TestAGVRobot(unittest.TestCase):
 
     def setUp(self):
@@ -118,6 +117,9 @@ class TestAGVRobot(unittest.TestCase):
         urdf_file = os.path.join(dirname,
                               'robot_descriptions', 'diff_drive_agv.urdf')
         self.robot = AGVRobot(urdf_file, self.start_position, self.start_orientation)
+
+    def tearDown(self):
+        p.disconnect()
 
     def test_get_and_set_world_state(self):
         # Set a new world state for the robot
@@ -158,9 +160,6 @@ class TestAGVRobot(unittest.TestCase):
             self.assertAlmostEqual(distance, correct_result[0])
             self.assertAlmostEqual(angle, correct_result[1])
             self.assertAlmostEqual(target_angle_error, correct_result[2])
-
-
-
 
 
 
@@ -216,12 +215,7 @@ class DiffDriveAGV(AGVRobot):
         p.setJointMotorControl2(self.urdf, self.left_wheel_index, p.VELOCITY_CONTROL)
         p.setJointMotorControl2(self.urdf, self.right_wheel_index, p.VELOCITY_CONTROL)
 
-
-
-
-
-
-    def __calculate_wheel_comands(self, linear_velocity: float, angular_velocity: float):
+    def _calculate_wheel_comands(self, linear_velocity: float, angular_velocity: float):
         """Calculates the wheel commands for a differential drive robot.
 
         Args:
@@ -256,7 +250,7 @@ class DiffDriveAGV(AGVRobot):
                                     -self.max_angular_velocity,
                                     self.max_angular_velocity)
 
-        wheel_commands = self.__calculate_wheel_comands(linear_velocity, angular_velocity)
+        wheel_commands = self._calculate_wheel_comands(linear_velocity, angular_velocity)
         p.setJointMotorControl2(self.urdf,
                                 self.left_wheel_index,
                                 p.VELOCITY_CONTROL,
@@ -266,9 +260,7 @@ class DiffDriveAGV(AGVRobot):
                                 p.VELOCITY_CONTROL,
                                 targetVelocity=wheel_commands[1])
 
-
     def standard_position_controller(self,distance,angle,target_angle_error):
-        print(distance,angle,target_angle_error)
         kp_lin=1
         kp_ang= 0.8
         kp_target_ang = 0.4
@@ -302,6 +294,95 @@ class DiffDriveAGV(AGVRobot):
             angular_velocity = 0
 
         return [linear_velocity, angular_velocity]
+
+
+
+class TestDiffDriveAGV(unittest.TestCase):
+
+    def setUp(self):
+
+        p.connect(p.DIRECT)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.loadURDF("plane.urdf")
+        p.setGravity(0, 0, -9.81)
+
+        self.start_position = np.array([0, 0, 0.4])
+        self.start_orientation = np.array([0, 0, 0,1])
+        dirname = os.path.dirname(__file__)
+        urdf_file = os.path.join(dirname,
+                              'robot_descriptions', 'diff_drive_agv.urdf')
+        self.robot = DiffDriveAGV(urdf_file, self.start_position, self.start_orientation,
+                                  "left_wheel_joint", "right_wheel_joint",
+                                  {"wheel_radius": 0.1,
+                                   "track_width": 0.5,
+                                   "max_linear_velocity": 1,
+                                   "max_angular_velocity": 1})
+
+
+
+    def tearDown(self) -> None:
+        p.disconnect()
+
+    def test_calculate_wheel_commands(self):
+
+        wheel_commands = self.robot._calculate_wheel_comands(1, 0)
+        self.assertTrue(np.allclose(wheel_commands, [10, 10]))
+
+
+        wheel_commands = self.robot._calculate_wheel_comands(0, 1)
+        self.assertTrue(np.allclose(wheel_commands, [-2.5, 2.5]))
+
+
+        wheel_commands = self.robot._calculate_wheel_comands(1, 1)
+        self.assertTrue(np.allclose(wheel_commands, [7.5, 12.5]))
+
+    def test_set_velocity(self):
+
+            #self.robot.set_velocity([1, 0])
+            for _ in range(80):
+                self.robot.set_velocity([1, 0])
+                p.stepSimulation()
+            # get the body velocity of the robot
+            body_velocity = p.getBaseVelocity(self.robot.urdf)[0]
+            print(body_velocity)
+            self.assertTrue(np.allclose(body_velocity, [1, 0, 0], atol=0.1))
+
+    def test_standard_position_controller(self):
+        # set the robot to the origin with zero orientation
+        self.robot.set_world_state(np.array([0, 0, 0]), np.array([0, 0, 0, 1]))
+
+        # set the target position to (1,1,0) and the target orientation to zero
+        target_position = np.array([2, 2, 0])
+        target_orientation = np.array([0, 0, 0, 1])
+        self.robot.set_target_pose(target_position, target_orientation)
+        for _ in range(100):
+            self.robot.update_position_loop()
+            for _ in range(50):
+                p.stepSimulation()
+            print(self.robot._calculate_position_error())
+        # calculate the position error
+        distance, angle, target_angle_error = self.robot._calculate_position_error()
+
+        # check if the position error converges to zero
+        self.assertAlmostEqual(distance, 0, delta=0.05)
+        self.assertAlmostEqual(target_angle_error, 0, delta=0.05)
+
+        # set the target position to (1,-1,0) and the target orientation to pi/2
+        target_position = np.array([1, -1, 0])
+        target_orientation = p.getQuaternionFromEuler([0, 0, np.pi/2])
+        self.robot.set_target_pose(target_position, target_orientation)
+        for _ in range(100):
+            self.robot.update_position_loop()
+            p.stepSimulation()
+        # calculate the position error
+        distance, angle, target_angle_error = self.robot._calculate_position_error()
+
+        # check if the position error is correct
+        self.assertAlmostEqual(distance, 0, delta=0.05)
+        self.assertAlmostEqual(target_angle_error, 0, delta=0.05)
+
+
+
 
 
 

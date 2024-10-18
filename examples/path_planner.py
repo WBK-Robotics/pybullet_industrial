8e11258526a1e96bd361c937d294e880d1d15fc0
philipp1604 <122354024+philipp1604@users.noreply.github.com>
2024-10-18 18:00:21 +0200

    Extracting Joint LImits
    - extracting joint limits of moveable joints

--- examples/path_planner.py
+++ examples/path_planner.py
@@ -1,11 +1,30 @@
 import pybullet_industrial as pi
+import pybullet as p
+import numpy as np
 
 
 class PathPlanner:
-    def __init__(self, robot: pi.RobotBase, step_size, max_iterations):
+    def __init__(self, robot: pi.RobotBase, step_size=None,
+                 max_iterations=None):
         self.robot = robot
         self.step_size = step_size
         self.max_iterations = max_iterations
+        self.number_of_joints = p.getNumJoints(robot.urdf)
+
+        self.lower_joint_limit = np.zeros(6)
+        self.upper_joint_limit = np.zeros(6)
+
+        index = 0
+        for joint_number in range(self.number_of_joints):
+            joint_type = p.getJointInfo(robot.urdf, joint_number)[2]
+
+            if joint_type == 0:
+                lower_limit = p.getJointInfo(robot.urdf, joint_number)[8]
+                upper_limit = p.getJointInfo(robot.urdf, joint_number)[9]
+                self.lower_joint_limit[index] = lower_limit
+                self.upper_joint_limit[index] = upper_limit
+                index += 1
+        print("s")
 
     def create_random_sample(self):
         pass
